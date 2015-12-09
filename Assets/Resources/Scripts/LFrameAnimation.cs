8898639ff43fd2fd087253020227fcee8c89282b
ljj <ljj@mail.com>
2015-12-09 21:30:44 +0800

    update richtext

---
+++ Assets/Resources/Scripts/LFrameAnimation.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace Lui
+{
+    public class LFrameAnimation
+    {
+        protected int currentIndex = 0;
+        protected Texture[] frameTex;
+        public float fps = 15f;
+        
+        protected float time;
+        protected int frameLenght;
+        protected Rect rect;
+        protected bool isPlaying = false;
+
+        private string _path;
+        public string path
+        {
+           set {
+               _path = value;
+               //load textures
+               Object[] texObj = Resources.LoadAll(_path);
+               frameTex = new Texture[texObj.Length];
+               texObj.CopyTo(frameTex, 0);
+               frameLenght = texObj.Length;
+
+               rect = new Rect(0, 0, frameTex[0].width, frameTex[0].height);
+           }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        public LFrameAnimation(string path)
+        {
+            
+        }
+
+        void OnGUI()
+        {
+            if (isPlaying)
+            {
+                drawAnimation();
+            }
+        }
+
+        // Update is called once per frame
+        protected void drawAnimation()
+        {
+            GUI.DrawTexture(rect, frameTex[currentIndex]);
+            if (currentIndex < frameLenght - 1)
+            {
+                time += Time.deltaTime;
+                if (time >= 1.0f / fps)
+                {
+                    currentIndex++;
+                    time = 0;
+                    if (currentIndex == frameLenght - 1)
+                    {
+                        currentIndex = 0;
+                    }
+                }
+            }
+        }
+
+        public void play()
+        {
+            isPlaying = true;
+        }
+
+        public void stop()
+        {
+            isPlaying = false;
+            currentIndex = 0;
+            GUI.DrawTexture(rect, frameTex[currentIndex]);
+        }
+
+        public void pause()
+        {
+            isPlaying = false;
+        }
+    }
+}
--- Assets/Resources/Scripts/LRichText.cs
+++ Assets/Resources/Scripts/LRichText.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
+using System.Security;
+using UnityEngine.UI;
 
 namespace Lui
 {
@@ -83,9 +84,9 @@ class LRichElementAnim : LRichElement
 /// <summary>
 /// 换行元素
 /// </summary>
-class TRichElementNewline : LRichElement
+class LRichElementNewline : LRichElement
 {
-    public TRichElementNewline()
+    public LRichElementNewline()
     {
         this.type = Type.NEWLINE;
     }
@@ -94,10 +95,14 @@ class TRichElementNewline : LRichElement
 /// <summary>
 /// 缓存结构
 /// </summary>
-class TRichCacheElement : Object
+class LRichCacheElement : Object
 {
     public bool isUse;
     public GameObject node;
+    public LRichCacheElement(GameObject node)
+    {
+        this.node = node;
+    }
 }
 
 /// <summary>
@@ -107,8 +112,8 @@ struct LRenderElement
 {
     public Type type;
     public string strChar;
-    public float width;
-    public float height;
+    public int width;
+    public int height;
     public bool isOutLine;
     public bool isUnderLine;
     public Font font;
@@ -132,31 +137,34 @@ struct LRenderElement
         cloneOjb.color = this.color;
         cloneOjb.data = this.data;
         cloneOjb.path = this.path;
-        cloneOjb.fp = this.fp;
+        cloneOjb.fs = this.fs;
         cloneOjb.isNewLine = this.isNewLine;
         cloneOjb.pos = this.pos;
         return cloneOjb;
     }
 }
 
-public class TRichText : MonoBehaviour, IRichTextClickableProtocol
+public class LRichText : MonoBehaviour, IRichTextClickableProtocol
 {
     public AlignType alignType { get; protected set; }
-    public float verticalSpace { get; protected set; }
-    public float maxLineWidth { get; protected set; }
-    public List<LRichElement> richElements { protected get; }
-    public List<LRenderElement> elemRenderArr { protected get; }
-    public List<TRichCacheElement> cacheLabElements { protected get; }
-    public List<TRichCacheElement> cacheImgElements { protected get; }
+    public int verticalSpace { get; protected set; }
+    public int maxLineWidth { get; protected set; }
+    public int realLineHeight { get; protected set; }
+    public int realLineWidth { get; protected set; }
+
+    List<LRichElement> richElements;
+    List<LRenderElement> elemRenderArr;
+    List<LRichCacheElement> cacheLabElements;
+    List<LRichCacheElement> cacheImgElements;
 
     public void removeAllElements()
     {
-        foreach(TRichCacheElement lab in cacheLabElements)
+        foreach(LRichCacheElement lab in cacheLabElements)
         {
             lab.isUse = false;
             lab.node.transform.SetParent(null);
         }
-        foreach (TRichCacheElement img in cacheImgElements)
+        foreach (LRichCacheElement img in cacheImgElements)
         {
             img.isUse = false;
             img.node.transform.SetParent(null);
@@ -180,14 +188,19 @@ public class TRichText : MonoBehaviour, IRichTextClickableProtocol
 
     public void insertElement(int newline)
     {
-        richElements.Add(new TRichElementNewline());
+        richElements.Add(new LRichElementNewline());
     }
 
-    public TRichText()
+    public LRichText()
     {
         this.alignType = AlignType.LEFT_TOP;
         this.verticalSpace = 0;
         this.maxLineWidth = 300;
+
+        richElements = new List<LRichElement>();
+        elemRenderArr = new List<LRenderElement>();
+        cacheLabElements = new List<LRichCacheElement>();
+        cacheImgElements = new List<LRichCacheElement>();
     }
 
     public void reloadData()
@@ -215,8 +228,8 @@ public class TRichText : MonoBehaviour, IRichTextClickableProtocol
 
                     TextGenerationSettings setting = new TextGenerationSettings();
                     setting.font = elemText.font;
-                    rendElem.width = gen.GetPreferredWidth(rendElem.strChar, setting);
-                    rendElem.height = gen.GetPreferredHeight(rendElem.strChar, setting);
+                    rendElem.width = (int)gen.GetPreferredWidth(rendElem.strChar, setting);
+                    rendElem.height = (int)gen.GetPreferredHeight(rendElem.strChar, setting);
                     elemRenderArr.Add(rendElem);
                 }
             }
@@ -261,10 +274,344 @@ public class TRichText : MonoBehaviour, IRichTextClickableProtocol
 
     protected void formarRenderers()
     {
-        float charWidth = 0;
-        float oneLine = 0;
+        int oneLine = 0;
         int lines = 1;
 
+        int len = elemRenderArr.Count;
+        for (int i = 0; i < len; i++)
+        {
+            LRenderElement elem = elemRenderArr[i];
+            if (elem.isNewLine) // new line
+            {
+                oneLine = 0;
+                lines++;
+                elem.width = 0;
+                elem.height = 27;
+                elem.pos = new Vector2(oneLine, -lines * 27);
+
+            }
+            else //other elements
+            {
+                if (oneLine + elem.width > maxLineWidth)
+                {
+                    if (elem.type == Type.TEXT)
+                    {
+                       if (isChinese(elem.strChar) || elem.strChar == " ")
+                       {
+                           oneLine = 0;
+                           lines++;
+
+                           elem.pos = new Vector2(oneLine, -lines * 27);
+                           oneLine = elem.width;
+                       }
+                       else // en
+                       {
+                           int spaceIdx = 0;
+                           int idx = i;
+                           while (idx > 0)
+                           {
+                               idx--;
+                               if (elemRenderArr[idx].strChar == " ")
+                               {
+                                   spaceIdx = idx;
+                                   break;
+                               }
+                           }
+                           // can't find space , force new line
+                           if (spaceIdx == 0)
+                           {
+                               oneLine = 0;
+                               lines++;
+                               elem.pos = new Vector2(oneLine, -lines * 27);
+                               oneLine = elem.width;
+                           }
+                           else
+                           {
+                               oneLine = 0;
+                               lines++;
+
+                               for (int _i = spaceIdx + 1; _i <= i; ++_i)
+                               {
+                                   LRenderElement _elem = elemRenderArr[_i];
+                                   _elem.pos = new Vector2(oneLine, -lines * 27);
+                                   oneLine += elem.width;
+                               }
+                           }
+                       }
+                    }else if (elem.type == Type.ANIM || elem.type == Type.IMAGE)
+                    {
+                        lines++;
+                        elem.pos = new Vector2(0, -lines * 27);
+                        oneLine = elem.width;
+                    }
+                }
+                else
+                {
+                    elem.pos = new Vector2(oneLine, -lines * 27);
+                    oneLine += elem.width;
+                }
+            }
+        }
+        //sort all lines
+        Dictionary<int,List<LRenderElement>> rendElemLineMap = new Dictionary<int,List<LRenderElement>>();
+        List<int> lineKeyList = new List<int>();
+        len = elemRenderArr.Count;
+        for (int i = 0; i < len ; i++ )
+        {
+            LRenderElement elem = elemRenderArr[i];
+            List<LRenderElement> lineList;
+            if (!rendElemLineMap.ContainsKey((int)elem.pos.y))
+            {
+                lineList = new List<LRenderElement>();
+                rendElemLineMap[(int)elem.pos.y] = lineList;
+            }
+            rendElemLineMap[(int)elem.pos.y].Add(elem);
+        }
+        // all lines in arr
+        List<List<LRenderElement>> rendLineArrs = new List<List<LRenderElement>>();
+        foreach (var item in rendElemLineMap)
+        {
+            lineKeyList.Add(-1 * item.Key);
+        }
+        lineKeyList.Sort();
+        len = lineKeyList.Count;
+        for (int i = 0; i < len; i++)
+        {
+            int posY = -1 * lineKeyList[i];
+            string lineString = "";
+            LRenderElement _lastEleme = rendElemLineMap[posY][0];
+            LRenderElement _lastDiffStartEleme = rendElemLineMap[posY][0];
+            if (rendElemLineMap[posY].Count > 0)
+            {
+                List<LRenderElement> lineElemArr = new List<LRenderElement>();
+
+                foreach (LRenderElement elem in rendElemLineMap[posY])
+                {
+                    if (_lastEleme.type == Type.TEXT && elem.type == Type.TEXT)
+                    {
+                        if (_lastEleme.color == elem.color)
+                        {
+                            // the same color can mergin one string
+                            lineString += elem.strChar;
+                        }
+                        else // diff color
+                        {
+                            if (_lastDiffStartEleme.type == Type.TEXT)
+                            {
+                                LRenderElement _newElem = _lastDiffStartEleme.Clone();
+                                _newElem.strChar = lineString;
+                                lineElemArr.Add(_newElem);
+
+                                _lastDiffStartEleme = elem;
+                                lineString = elem.strChar;
+                            }
+                        }
+                    }
+                    else if (elem.type == Type.IMAGE || elem.type == Type.ANIM || elem.type == Type.NEWLINE)
+                    {
+                        //interrupt
+                        if (_lastDiffStartEleme.type == Type.TEXT)
+                        {
+                            LRenderElement _newEleme = _lastDiffStartEleme.Clone();
+                            _newEleme.strChar = lineString;
+                            lineString = "";
+                            lineElemArr.Add(_newEleme);
+                        }
+                        lineElemArr.Add(elem);
+
+                    }else if (_lastEleme.type != Type.TEXT)
+                    {
+                        //interrupt
+                        _lastDiffStartEleme = elem;
+                        if (elem.type == Type.TEXT)
+                        {
+                            lineString = elem.strChar;
+                        }
+                    }
+                    _lastEleme = elem;
+                }
+                // the last elementText
+                if (_lastDiffStartEleme.type == Type.TEXT)
+                {
+                    LRenderElement _newElem = _lastDiffStartEleme.Clone();
+                    _newElem.strChar = lineString;
+                    lineElemArr.Add(_newElem);
+                }
+                rendLineArrs.Add(lineElemArr);
+            }
+        }
+
+        // offset position
+        int _offsetLineY = 0;
+        realLineHeight = 0;
+        len = rendLineArrs.Count;
+        for (int i = 0; i < len; i++ )
+        {
+            List<LRenderElement> _lines = rendLineArrs[i];
+            int _lineHeight = 0;
+            foreach (LRenderElement elem in _lines)
+            {
+                _lineHeight = Mathf.Max(_lineHeight, elem.height);
+            }
+            realLineHeight += _lineHeight;
+            _offsetLineY += (_lineHeight - 27);
+
+            for (int j = 0; j < _lines.Count; j++ )
+            {
+                LRenderElement elem = _lines[j];
+                elem.pos = new Vector2(elem.pos.x, elem.pos.y - _offsetLineY);
+                realLineHeight = Mathf.Max(realLineHeight, (int)Mathf.Abs(elem.pos.y));
+            }
+        }
+    
+        // place all position
+        realLineWidth = 0;
+        GameObject obj = null;
+        foreach (List<LRenderElement> _lines in rendLineArrs)
+        {
+            int _lineWidth = 0;
+            foreach (LRenderElement elem in _lines)
+            {
+                if (elem.type != Type.NEWLINE)
+                {
+                    if (elem.type == Type.TEXT)
+                    {
+                        obj = getCacheLabel();
+                        makeLabel(obj, elem);
+                    }
+                    else if (elem.type == Type.IMAGE)
+                    {
+                        obj = getCacheImage();
+                        makeImage(obj, elem);
+                    }
+                    else if (elem.type == Type.ANIM)
+                    {
+                        obj = getCacheImage();
+                        makeImage(obj, elem);
+                    }
+                    _lineWidth += elem.width;
+                    obj.transform.SetParent(transform);
+                    obj.transform.position = new Vector2(elem.pos.x, elem.pos.y + realLineHeight);
+                }
+            }
+            realLineWidth = Mathf.Max(_lineWidth, realLineWidth);
+        }
+
+        RectTransform rtran = this.GetComponent<RectTransform>();
+        //align
+        if (alignType == AlignType.DESIGN_CENTER)
+        {
+            rtran.anchoredPosition = new Vector2(0.5f, 0.5f);
+            rtran.sizeDelta = new Vector2(maxLineWidth, realLineHeight);
+
+        }else if (alignType == AlignType.REAL_CENTER)
+        {
+            rtran.anchoredPosition = new Vector2(0.5f, 0.5f);
+            rtran.sizeDelta = new Vector2(realLineWidth, realLineHeight);
+
+        }else if (alignType == AlignType.LEFT_TOP)
+        {
+            rtran.anchoredPosition = new Vector2(0, 1);
+            rtran.sizeDelta = new Vector2(realLineWidth, realLineHeight);
+        }
+    }
+
+    void makeLabel(GameObject lab,LRenderElement elem)
+    {
+        Text comText = lab.GetComponent<Text>();
+        if (comText != null)
+        {
+            comText.text = elem.strChar;
+            comText.font = elem.font;
+
+            TextGenerator gen = new TextGenerator();
+            TextGenerationSettings setting = new TextGenerationSettings();
+            setting.font = elem.font;
+            elem.width = (int)gen.GetPreferredWidth(elem.strChar, setting);
+        }
+    }
+
+    void makeImage(GameObject img, LRenderElement elem)
+    {
+        Image comImage = img.GetComponent<Image>();
+        if (comImage != null)
+        {
+            Texture2D tex = Resources.Load(elem.path) as Texture2D;
+            comImage.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height),Vector2.zero);
+        }
+    }
+
+    void makeAnim(GameObject anim, LRenderElement elem)
+    {
+        LFrameAnimation comFram = anim.GetComponent<LFrameAnimation>();
+        if (comFram != null)
+        {
+            comFram.path = elem.path;
+            comFram.fps = elem.fs;
+            comFram.play();
+        }
+    }
+
+    protected GameObject getCacheLabel()
+    {
+        GameObject ret = null;
+        int len = cacheLabElements.Count;
+        for (int i = 0; i < len;i++ )
+        {
+            LRichCacheElement cacheElem = cacheLabElements[i];
+            if (cacheElem.isUse == false)
+            {
+                cacheElem.isUse = true;
+                ret = cacheElem.node;
+                break;
+            }
+        }
+        if (ret == null)
+        {
+            ret = new GameObject();
+            Text comp = ret.AddComponent<Text>();
+            comp.alignment = TextAnchor.LowerLeft;
+            cacheLabElements.Add(new LRichCacheElement(ret));
+        }
+        return ret;
+    }
+
+    protected GameObject getCacheImage()
+    {
+        GameObject ret = null;
+        int len = cacheLabElements.Count;
+        for (int i = 0; i < len; i++)
+        {
+            LRichCacheElement cacheElem = cacheLabElements[i];
+            if (cacheElem.isUse == false)
+            {
+                cacheElem.isUse = true;
+                ret = cacheElem.node;
+                break;
+            }
+        }
+        if (ret == null)
+        {
+            ret = new GameObject();
+            ret.AddComponent<Image>();
+            cacheLabElements.Add(new LRichCacheElement(ret));
+        }
+        return ret;
+    }
+
+    protected bool isChinese(string text)
+    {
+        bool hasChinese = false;
+        char[] c = text.ToCharArray();
+        for (int i = 0; i < c.Length; i++)
+        {
+            if (c[i] >= 0x4e00 && c[i] <= 0x9fbb)
+            {
+                hasChinese = true;
+                break;
+            }
+        }
+        return hasChinese;
     }
 
 	// Use this for initialization
