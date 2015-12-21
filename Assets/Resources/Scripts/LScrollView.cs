0f0ed5fede9ebf5a780b7bb6af1f1cda7c0b2536
ljj <ljj@mail.com>
2015-12-22 01:08:15 +0800

    update dev

--- Assets/Resources/Scripts/LScrollView.cs
+++ Assets/Resources/Scripts/LScrollView.cs
@@ -19,31 +19,22 @@ namespace Lui
     public class LScrollView : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
     {
         protected const float MOVE_OVERFLOW = 10f;
-        protected const float MOVE_EASEIN_RATE = 0.5f;
         protected const float RELOCATE_DURATION = 0.2f;
-        protected const float DEACCELERATE_INTERVAL = 0.245f;
 
         public bool bounceable;
-        public bool deaccelerateable;
         public GameObject container;
         public ScrollDirection direction;
         private Vector2 lastMovePoint;
         private Vector2 maxOffset;
         private Vector2 minOffset;
-        private bool deaccelerateScrolling;
-        private bool animatedScrolling;
-        protected float dragSpeed;
         protected Vector2 scrollDistance;
-        protected Vector2 touchBeganPoint;
         public bool dragable;
 
         public LScrollView()
         {
             direction = ScrollDirection.BOTH;
             lastMovePoint = Vector2.zero;
-            bounceable = false;
-            deaccelerateable = true;
-            dragSpeed = 0;
+            bounceable = true;
             scrollDistance = Vector2.zero;
             dragable = true;
             maxOffset = Vector2.zero;
@@ -82,24 +73,17 @@ namespace Lui
         public void OnPointerDown(PointerEventData eventData)
         {
             Vector2 point = transform.InverseTransformPoint(eventData.position);
-            touchBeganPoint = point;
             if (dragable)
             {
                 lastMovePoint = point;
-                stoppedDeaccelerateScroll();
-                stoppedAnimatedScroll();
+                iTween.Stop(container);
+                onScrolling();
             }
         }
 
         public void OnDrag(PointerEventData eventData)
         {
             Vector2 point = transform.InverseTransformPoint(eventData.position);
-            float distance = Vector2.Distance(point, touchBeganPoint);
-            if (distance < MOVE_OVERFLOW)
-            {
-               
-            }
-            
             if (dragable)
             {
                 scrollDistance = point - lastMovePoint;
@@ -125,37 +109,21 @@ namespace Lui
         {
             if (dragable)
             {
-                if (deaccelerateable && direction != ScrollDirection.BOTH && Time.deltaTime < DEACCELERATE_INTERVAL)
+                Vector2 offset = getContentOffset();
+                if (validateOffset(ref offset))
                 {
-                    Vector2 endPoint = transform.InverseTransformPoint(eventData.position);
-                    switch (direction)
-                    {
-                        case ScrollDirection.HORIZONTAL:
-                            dragSpeed = Mathf.Abs(touchBeganPoint.x - endPoint.x) / Time.deltaTime;
-                            break;
-                        default:
-                            dragSpeed = Mathf.Abs(touchBeganPoint.y - endPoint.y) / Time.deltaTime;
-                            break;
-                    }
+                    relocateContainerWithoutCheck(offset);
                 }
                 else
                 {
-                    Vector2 offset = getContentOffset();
-                    if (validateOffset(ref offset))
-                    {
-                        relocateContainerWithoutCheck(offset);
-                    }
-                    else
-                    {
-                        onDraggingScrollEnded();
-                    }
+                    onDraggingScrollEnded();
                 }
             }
         }
 
         protected void relocateContainerWithoutCheck(Vector2 offset)
         {
-            setContentOffsetEaseInWithoutCheck(offset, RELOCATE_DURATION, MOVE_EASEIN_RATE);
+            setContentOffsetEaseInWithoutCheck(offset, RELOCATE_DURATION);
         }
 
         protected void relocateContainer()
@@ -163,18 +131,21 @@ namespace Lui
             Vector2 offset = getContentOffset();
             if (validateOffset(ref offset))
             {
-                setContentOffsetEaseInWithoutCheck(offset, RELOCATE_DURATION, MOVE_EASEIN_RATE);
+                setContentOffsetEaseInWithoutCheck(offset, RELOCATE_DURATION);
             }
         }
 
-        protected void setContentOffsetEaseInWithoutCheck(Vector2 offset, float duration, float rate)
+        protected void setContentOffsetEaseInWithoutCheck(Vector2 offset, float duration)
         {
             iTween.Stop(container);
             iTween.MoveTo(container, iTween.Hash(
                 "position", transform.TransformPoint(offset), 
                 "time", duration,
                 "easetype",iTween.EaseType.easeInQuad,
-                "oncomplete", "stoppedAnimatedScroll"));
+                "onupdate","onScrolling",
+                "onupdatetarget",gameObject,
+                "oncomplete", "onMoveComplete",
+                "oncompletetarget",gameObject));
 
             onScrolling();
         }
@@ -185,7 +156,7 @@ namespace Lui
             {
                 validateOffset(ref offset);
             }
-            setContentOffsetEaseInWithoutCheck(offset, duration, rate);
+            setContentOffsetEaseInWithoutCheck(offset, duration);
         }
 
         public void setContentOffset(Vector2 offset)
@@ -194,19 +165,33 @@ namespace Lui
             {
                 validateOffset(ref offset);
             }
+            else
+            {
+                validateOffset(ref offset, new Vector2(0, 100));
+            }
             iTween.Stop(container);
+            container.transform.localPosition = offset;
+            onScrolling();
+        }
+
+        public void setContentOffsetWithoutCheck(Vector2 offset)
+        {
             container.transform.localPosition = offset;
             onScrolling();
         }
 
         protected bool validateOffset(ref Vector2 point)
         {
-            float x = point.x; 
-            float y = point.y;
-            x = Mathf.Max(x, minOffset.x);
-            x = Mathf.Min(x, maxOffset.x);
-            y = Mathf.Max(y, minOffset.y);
-            y = Mathf.Min(y, maxOffset.y);
+            return validateOffset(ref point, Vector2.zero);
+        }
+
+        protected bool validateOffset(ref Vector2 point, Vector2 append)
+        {
+            float x = point.x, y = point.y;
+            x = Mathf.Max(x, minOffset.x );
+            x = Mathf.Min(x, maxOffset.x + append.x);
+            y = Mathf.Max(y, minOffset.y );
+            y = Mathf.Min(y, maxOffset.y + append.y);
 
             if (point.x != x || point.y != y)
             {
@@ -225,15 +210,9 @@ namespace Lui
             return container.transform.localPosition;
         }
 
-        protected void stoppedDeaccelerateScroll()
+        protected void onMoveComplete()
         {
-            iTween.Stop(container);
-        }
-
-        protected void stoppedAnimatedScroll()
-        {
-            iTween.Stop(container);
-            onScrolling();
+            Debug.Log("onMoveComplete");
         }
 
         void Awake()
@@ -243,13 +222,13 @@ namespace Lui
 
         protected void onScrolling()
         {
-
+            Debug.Log("onScrolling");
 
         }
 
         protected void onDraggingScrollEnded()
         {
-
+            Debug.Log("onDraggingScrollEnded");
 
         }
 
