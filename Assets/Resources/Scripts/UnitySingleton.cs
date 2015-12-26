c9af4416f0e3848d5bc139c5c24d7f32e466536d
ljj <ljj@mail.com>
2015-12-26 21:05:40 +0800

    update dev

---
+++ Assets/Resources/Scripts/UnitySingleton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+public class UnitySingleton<T> : MonoBehaviour
+        where T : Component
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(T)) as T;
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    //obj.hideFlags = HideFlags.DontSave;
+                    obj.hideFlags = HideFlags.HideAndDontSave;
+                    _instance = (T)obj.AddComponent(typeof(T));
+                }
+            }
+            return _instance;
+        }
+    }
+    public virtual void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
