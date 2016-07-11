6bee6abb71b6e1f18ed2eee2b0ef2eb6fc889a85
lingjijian <342854406@qq.com>
2016-07-11 10:53:59 +0800

    update net

---
+++ Assets/Plugins/Slua_Managed/ByteArray.cs
@@ -0,0 +1,21 @@
+
+
+namespace SLua
+{
+	//ByteArray
+	public class ByteArray
+	{
+		public byte[] data = null;
+
+		public ByteArray()
+		{
+			//
+		}
+
+		public ByteArray(byte[] d)
+		{
+			this.data = d;
+		}
+	}
+}
+
